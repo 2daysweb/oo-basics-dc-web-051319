18b3fba51b2e0137c5e5a6eb9d4617e24939ca1b
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-17 20:37:23 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,8 +7,12 @@ def initialize(brand)
 end 
 
 def cobble
+<<<<<<< HEAD
   self.condition = "new"
   puts "Your shoe is as good as new!"
+=======
+  puts ""
+>>>>>>> 76a46deb12a0422713bfeef1b02f5e9344210729
 end 
 
 end 
